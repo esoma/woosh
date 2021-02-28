2dc9c9452bc0bbd6e0dfdc353e9030a9e85ef0bb
Erik Soma <stillusingirc@gmail.com>
2021-02-28 13:05:57 -0500

    Remove C test binaries and add a contrived test for a specific branch in the fifobuffer.

--- test/c/test_fifobuffer.c
+++ test/c/test_fifobuffer.c
@@ -141,6 +141,45 @@ int main()
 
     fifo_buffer_delete(&fb);
     
+    // this test part of the test specifically manipulates some of the inner
+    // workings of the fifo buffer to cover a specific and pretty unusual
+    // scenario
+    
+    // in this case we're going to have a buffer with an initial size of 3
+    // chars, so 3 items can fit in the buffer
+    if (!fifo_buffer_new(&fb, sizeof(char) * 3))
+    {
+        fprintf(stderr, "failed to create buffer\n");
+        return EXIT_FAILURE;
+    }
+    
+    // we'll fill the buffer so that all three items are in it
+    // internally our buffer should look (something) like this: a b c
+    if (!fifo_buffer_push(&fb, "abc", sizeof(char) * 3))
+    {
+        fprintf(stderr, "failed to add 'abc' to buffer\n");
+        return EXIT_FAILURE;
+    }
+    
+    // then we'll pop the first two items out, internally our buffer should
+    // look (something) like this: _ _ c
+    fifo_buffer_pop(&fb, 2);
+    
+    // now we'll add one character to the buffer
+    // the internal buffer is large enough to hold the character, but it must
+    // be kept sequential, so it will be changed to: c d _
+    //
+    // this should cover a specific branch internally that will move c, rather
+    // than copy it because it doesn't overlap in the buffer
+    if (!fifo_buffer_push(&fb, "d", sizeof(char) * 1))
+    {
+        fprintf(stderr, "failed to add 'd' to buffer\n");
+        return EXIT_FAILURE;
+    }
+    
+    // done with this specific internal case
+    fifo_buffer_delete(&fb);
+    
     fprintf(stderr, "passed\n");
     return EXIT_SUCCESS;
 }
