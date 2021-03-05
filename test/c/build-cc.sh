a85e7e7313d638d1c0d1d974b87a1bb15ee4404a
Erik Soma <stillusingirc@gmail.com>
2021-03-05 16:50:52 -0500

    Fix c test builder error reporting for linux and revert some changes to the github linux workflow.

--- test/c/build-cc.sh
+++ test/c/build-cc.sh
@@ -10,16 +10,14 @@ fi
 
 mkdir -p build
 if [[ $? != 0 ]]; then
-    RETVAL=$?
     echo "Failed to create build directory."
-    exit $RETVAL
+    exit 1
 fi
 
 PYTHON_INCLUDES=$(python3-config --includes)
 if [[ $? != 0 ]]; then
-    RETVAL=$?
     echo "Failed to get python include compilation flags."
-    exit $RETVAL
+    exit 1
 fi
 echo "    PYTHON_INCLUDES=${PYTHON_INCLUDES}"
 
@@ -27,9 +25,8 @@ PYTHON_LIBS=$(python3-config --libs --embed)
 if [[ $? != 0 ]]; then
     PYTHON_LIBS=$(python3-config --libs)
     if [[ $? != 0 ]]; then
-        RETVAL=$?
         echo "Failed to get python lib compilation flags."
-        exit $RETVAL
+        exit 1
     fi
 fi
 echo "    PYTHON_LIBS=${PYTHON_LIBS}"
@@ -37,9 +34,8 @@ echo "    PYTHON_LIBS=${PYTHON_LIBS}"
 PYTHON_LIBDIRS=""
 _=$(python3-config --ldflags);
 if [[ $? != 0 ]]; then
-    RETVAL=$?
     echo "Failed to get python lib compilation flags."
-    exit $RETVAL
+    exit 1
 fi
 for LDFLAG in $_; do
     if [[ ${LDFLAG} == -L* ]]; then
@@ -50,17 +46,15 @@ echo "    PYTHON_LIBDIRS=${PYTHON_LIBDIRS}"
 
 WOOSH_LIB_DIR=$(python3 -c "import _woosh; import pathlib; print(pathlib.Path(_woosh.__file__).parent)")
 if [[ $? != 0 ]]; then
-    RETVAL=$?
     echo "Failed to get woosh lib directory."
-    exit $RETVAL
+    exit 1
 fi
 echo "    WOOSH_LIB_DIR=${WOOSH_LIB_DIR}"
 
 WOOSH_LIB=$(python3 -c "import _woosh; import pathlib; print(pathlib.Path(_woosh.__file__).name)")
 if [[ $? != 0 ]]; then
-    RETVAL=$?
     echo "Failed to get woosh lib name."
-    exit $RETVAL
+    exit 1
 fi
 echo "    WOOSH_LIB=${WOOSH_LIB}"
 
@@ -69,9 +63,8 @@ echo "Discovering tests to build..."
 
 API_TESTS=$(find ./api -name "test_*.c")
 if [[ $? != 0 ]]; then
-    RETVAL=$?
     echo "Failed to find API tests."
-    exit $RETVAL
+    exit 1
 fi
 for TEST in $API_TESTS; do
     echo "    $TEST"
@@ -79,9 +72,8 @@ done
 
 INTERNAL_TESTS=$(find ./internal -name "test_*.c")
 if [[ $? != 0 ]]; then
-    RETVAL=$?
     echo "Failed to find internal tests."
-    exit $RETVAL
+    exit 1
 fi
 for TEST in $INTERNAL_TESTS; do
     echo "    $TEST"
@@ -93,9 +85,8 @@ for TEST in $API_TESTS; do
 
     TEST_NAME=$(basename $TEST .c)
     if [[ $? != 0 ]]; then
-        RETVAL=$?
         echo "Failed to get test name."
-        exit $RETVAL
+        exit 1
     fi
     
     echo "    Building $TEST_NAME..."
@@ -105,9 +96,8 @@ for TEST in $API_TESTS; do
         ${PYTHON_LIBDIRS} ${PYTHON_INCLUDES} -L ${WOOSH_LIB_DIR}\
         api/base.c "${TEST}" -l:"${WOOSH_LIB}" ${PYTHON_LIBS}
     if [[ $? != 0 ]]; then
-        RETVAL=$?
         echo "Failed to build test."
-        exit $RETVAL
+        exit 1
     fi
     
 done
@@ -118,18 +108,16 @@ for TEST in $INTERNAL_TESTS; do
 
     TEST_NAME=$(basename $TEST .c)
     if [[ $? != 0 ]]; then
-        RETVAL=$?
         echo "Failed to get test name."
-        exit $RETVAL
+        exit 1
     fi
     
     echo "    Building $TEST_NAME..."
     
     "$CC" ${_CC_COV_FLAGS} -o "build/${TEST_NAME}" -I ../../src/_woosh/ "${TEST}"
     if [[ $? != 0 ]]; then
-        RETVAL=$?
         echo "Failed to build test."
-        exit $RETVAL
+        exit 1
     fi
     
 done
